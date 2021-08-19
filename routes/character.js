35f98d9dc7d8150bb85b51e9d55186890db9cab1
Remi-deronzier <deronzier.remi@gmail.com>
2021-08-19 12:44:29 +0200

    manage pagination issues on server side

--- routes/character.js
+++ routes/character.js
@@ -7,17 +7,34 @@ const axios = require("axios");
 router.get("/characters", async (req, res) => {
   console.log("route: /characters");
   console.log(req.query);
-  const { limit, skip, name } = req.query;
-  const queryParams = qs.stringify({
+  const { limit, skip, name, currentPage } = req.query;
+  let queryParams = qs.stringify({
     limit: limit,
     skip: skip,
     name: name,
     apiKey: process.env.API_KEY_MARVEL,
   });
   try {
-    const response = await axios.get(
+    let response = await axios.get(
       `https://lereacteur-marvel-api.herokuapp.com/characters?${queryParams}`
     );
+    const pageCount = Math.ceil(response.data.count / limit);
+    const isCurrentPageExceeded = Number(currentPage) > pageCount; // manage the following problems:
+    // - a user changes the limit but is on a high page number which would render nothing
+    // - a user makes a research but is on a high page number which would render nothing
+    // To prevent to render nothing, we redirect the user on the first page by setting the variable `skip` to 0
+    if (isCurrentPageExceeded) {
+      queryParams = qs.stringify({
+        limit: limit,
+        skip: 0,
+        name: name,
+        apiKey: process.env.API_KEY_MARVEL,
+      });
+      response = await axios.get(
+        `https://lereacteur-marvel-api.herokuapp.com/characters?${queryParams}`
+      );
+    }
+    response.data.currentPage = isCurrentPageExceeded ? 0 : currentPage;
     res.status(200).json(response.data);
   } catch (error) {
     console.error(error.message);
--- routes/comic.js
+++ routes/comic.js
@@ -7,17 +7,35 @@ const axios = require("axios");
 router.get("/comics", async (req, res) => {
   console.log("route: /comics");
   console.log(req.query);
-  const { limit, skip, title } = req.query;
-  const queryParams = qs.stringify({
+
+  const { limit, skip, title, currentPage } = req.query;
+  let queryParams = qs.stringify({
     limit: limit,
     skip: skip,
     title: title,
     apiKey: process.env.API_KEY_MARVEL,
   });
   try {
-    const response = await axios.get(
+    let response = await axios.get(
       `https://lereacteur-marvel-api.herokuapp.com/comics?${queryParams}`
     );
+    const pageCount = Math.ceil(response.data.count / limit);
+    const isCurrentPageExceeded = Number(currentPage) > pageCount; // manage the following problems:
+    // - a user changes the limit but is on a high page number which would render nothing
+    // - a user makes a research but is on a high page number which would render nothing
+    // To prevent to render nothing, we redirect the user on the first page by setting the variable `skip` to 0
+    if (isCurrentPageExceeded) {
+      queryParams = qs.stringify({
+        limit: limit,
+        skip: 0,
+        title: title,
+        apiKey: process.env.API_KEY_MARVEL,
+      });
+      response = await axios.get(
+        `https://lereacteur-marvel-api.herokuapp.com/comics?${queryParams}`
+      );
+    }
+    response.data.currentPage = isCurrentPageExceeded ? 0 : currentPage;
     res.status(200).json(response.data);
   } catch (error) {
     console.error(error.message);
